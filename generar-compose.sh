f259cf836205f70ce44169b32140d918d48f5d69
TheoMiguel <tmiguel@fi.uba.ar>
2025-03-17 11:05:53 -0300

    ejercicio 1: generar-compose.sh

---
+++ generar-compose.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Validate input parameters
+if [ "$#" -ne 2 ]; then
+    echo "Uso: $0 <nombre_del_archivo> <cantidad_de_clientes>"
+    exit 1
+fi
+
+OUTPUT_FILE=$1
+CLIENT_COUNT=$2
+
+# Create the Docker Compose file content
+cat <<EOL > $OUTPUT_FILE
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOL
+
+for i in $(seq 1 $CLIENT_COUNT); do
+    cat <<EOL >> $OUTPUT_FILE
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOL
+done
+
+cat <<EOL >> $OUTPUT_FILE
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOL
+
