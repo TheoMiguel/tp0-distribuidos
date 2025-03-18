c209ad8a775d5f3e215a41e1b45b28303ca97ba8
TheoMiguel <tmiguel@fi.uba.ar>
2025-03-17 23:57:22 -0300

    added volumes to generar-compose.sh

--- generar-compose.sh
+++ generar-compose.sh
@@ -20,6 +20,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 EOL
@@ -33,6 +35,8 @@ for i in $(seq 1 $CLIENT_COUNT); do
     environment:
       - CLI_ID=$i
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
